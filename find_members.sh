52a3985c1c85cc0088f4c121ddc6c8badd027ad7
Kevin Brunelle <brunellek@gmail.com>
2015-06-10 14:35:58 -0400

    Turned the code into functions. Use a case statement to handle arguments.

--- find_members.sh
+++ find_members.sh
@@ -5,25 +5,30 @@ DATA=${HOME}/Documents/Low-Carb-Daily-Member-List/data
 BASE="http://community.myfitnesspal.com/en/group/members/394-low-carber-daily-forum-the-lcd-group/"
 MAX=544;
 
-# Move into the data directory
-cd $DATA
-
-if [ 0 -eq $# -o  "$1" = "--help" ]; then
+usage() {
 	echo "Usage:"
-	echo "	Fetch new files: "$0" --fetch [start] [end]"
-	echo "	Search for memb: "$0" [name] [name] [name]" 
-else
-	if [ "$1" = "--fetch" ]; then
+	echo "	Fetch new files:	"$0" --fetch [start] [end]"
+	echo "	Search for members:	"$0" [name] [name] [name]" 
+	echo "	Pages by member amount:	"$0" number_of_members"
+}
+
+count_pages() {
+	if [ $1 -gt 0 ]; then
+		dc -e "[la1+sa]sb $1 30/sa$1 30%0!=blap"
+	fi
+}
+
+fetch_pages() {
 		#
 		# This first block grabs all the member pages
 		#
-		if [ $# -gt 1 ]; then
-			min=$2;
+		if [ $# -gt 0 ]; then
+			min=$1;
 		else
 			min=1;
 		fi
-		if [ $# -gt 2 ]; then
-			max=$3;
+		if [ $# -gt 1 ]; then
+			max=$2;
 		else
 			max=${MAX};
 		fi
@@ -34,23 +39,49 @@ else
 			((PAGE++))
 			ftp $URL
 		done
-	else
-		#
-		# This sections looks for the named members.
-		# It looks for every name listed on the command line.
-		# I could probably use $* as I think MFP doesn't allow spaces in usernames
-		#
-		max=${MAX}
-		min=1
-		for NAME in $@
+}
+
+search_members() {
+	#
+	# This sections looks for the named members.
+	# It looks for every name listed on the command line.
+	# I could probably use $* as I think MFP doesn't allow spaces in usernames
+	#
+	max=${MAX}
+	min=1
+	for NAME in $@
+	do
+		i=$max
+		while (( i >= $min ))
 		do
-			i=$max
-			while (( i >= $min ))
-			do
-				file="p${i}?filter=members"
-				grep -q "\"Card Card-Member\"><a title=\"${NAME}" $file && i=$min && echo "${NAME} is on page ${BASE}${file}"
-				((i--))
-			done
+			file="p${i}?filter=members"
+			grep -q "\"Card Card-Member\"><a title=\"${NAME}" $file && i=$min && echo "${NAME} is on page ${BASE}${file}"
+			((i--))
 		done
-	fi
+	done
+}
+
+# Move into the data directory
+cd $DATA
+
+
+if [ 0 -eq $# ]; then
+	usage
+else
+	case $1 in
+		--help)
+			usage;
+			;;
+		--count)
+			shift;
+			count_pages $@;
+			;;
+		--fetch)
+			shift;
+			fetch_pages $@;
+			;;
+		*)
+			search_members $@;
+			;;
+	esac
 fi
